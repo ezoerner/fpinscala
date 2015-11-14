98c7cebff83caf2b47a5eafb87cf48e32e11e938
Eric Zoerner <eric.zoerner@gmail.com>
2015-11-14 11:57:02 -0800

    5.8 - 5.9 constant, from, fibs

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -1,95 +1,98 @@
 package fpinscala.laziness
 
 import Stream._
+
+import scala.annotation.tailrec
+
 sealed trait Stream[+A] {
 
   override def toString: String = toList.toString
 
   def toList: List[A] = this match {
-    case Empty => Nil
-    case Cons(h, t) => h() :: t().toList
+    case Empty      ⇒ Nil
+    case Cons(h, t) ⇒ h() :: t().toList
   }
 
-  def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
+  def foldRight[B](z: ⇒ B)(f: (A, ⇒ B) ⇒ B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
-      case _ => z
+      case Cons(h, t) ⇒ f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case _          ⇒ z
     }
 
-  def exists(p: A => Boolean): Boolean =
-    foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
+  def exists(p: A ⇒ Boolean): Boolean =
+    foldRight(false)((a, b) ⇒ p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
-  final def find(f: A => Boolean): Option[A] = this match {
-    case Empty => None
-    case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
+  final def find(f: A ⇒ Boolean): Option[A] = this match {
+    case Empty      ⇒ None
+    case Cons(h, t) ⇒ if (f(h())) Some(h()) else t().find(f)
   }
 
   def take(n: Int): Stream[A] =
     if (n == 0)
       Empty
     else this match {
-      case Empty => Empty
-      case Cons(h, t) => Cons(h, () => t() take (n - 1))
+      case Empty      ⇒ Empty
+      case Cons(h, t) ⇒ Cons(h, () ⇒ t() take (n - 1))
     }
 
   def drop(n: Int): Stream[A] =
     if (n == 0)
       this
     else this match {
-      case Empty => Empty
-      case Cons(h, t) => t() drop (n - 1)
+      case Empty      ⇒ Empty
+      case Cons(h, t) ⇒ t() drop (n - 1)
     }
 
-  def takeWhile(p: A => Boolean): Stream[A] = this match {
-    case Cons(h, t) if p(h()) => Cons(h, () => t() takeWhile p)
-    case _ => Empty
+  def takeWhile(p: A ⇒ Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) ⇒ Cons(h, () ⇒ t() takeWhile p)
+    case _                    ⇒ Empty
   }
 
-  def takeWhileUsingFoldRight(p: A => Boolean): Stream[A] =
-    foldRight(empty[A])((a, b) => if (p(a)) cons(a, b) else empty)
+  def takeWhileUsingFoldRight(p: A ⇒ Boolean): Stream[A] =
+    foldRight(empty[A])((a, b) ⇒ if (p(a)) cons(a, b) else empty)
 
-  def forAll(p: A => Boolean): Boolean =
-    foldRight(true)((a, b) => p(a) && b)
+  def forAll(p: A ⇒ Boolean): Boolean =
+    foldRight(true)((a, b) ⇒ p(a) && b)
 
   def headOption: Option[A] = this match {
-    case Empty => None
-    case Cons(h, t) => Some(h())
+    case Empty      ⇒ None
+    case Cons(h, t) ⇒ Some(h())
   }
 
   def headOptionUsingFoldRight: Option[A] =
-    foldRight(Option.empty[A])((a,b) => Some(a))
+    foldRight(Option.empty[A])((a, b) ⇒ Some(a))
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
 
-  def map[B](f: A => B): Stream[B] =
-    foldRight(empty[B])((elm, accum) => cons(f(elm), accum))
+  def map[B](f: A ⇒ B): Stream[B] =
+    foldRight(empty[B])((elm, accum) ⇒ cons(f(elm), accum))
 
-  def filter(p: A => Boolean): Stream[A] =
-    foldRight(empty[A]) { (elm, accum) =>
+  def filter(p: A ⇒ Boolean): Stream[A] =
+    foldRight(empty[A]) { (elm, accum) ⇒
       if (p(elm))
         cons(elm, accum)
       else
         accum
     }
 
-  def append[B>:A](as: => Stream[B]): Stream[B] =
-    foldRight(as)((elm, accum) => cons(elm, accum))
+  def append[B >: A](as: ⇒ Stream[B]): Stream[B] =
+    foldRight(as)((elm, accum) ⇒ cons(elm, accum))
 
-  def flatMap[B](f: A => Stream[B]): Stream[B] =
-    foldRight(empty[B])((elm, accum) => f(elm) append accum)
+  def flatMap[B](f: A ⇒ Stream[B]): Stream[B] =
+    foldRight(empty[B])((elm, accum) ⇒ f(elm) append accum)
 
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
 }
 case object Empty extends Stream[Nothing]
-case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+case class Cons[+A](h: () ⇒ A, t: () ⇒ Stream[A]) extends Stream[A]
 
 object Stream {
-  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+  def cons[A](hd: ⇒ A, tl: ⇒ Stream[A]): Stream[A] = {
     lazy val head = hd
     lazy val tail = tl
-    Cons(() => head, () => tail)
+    Cons(() ⇒ head, () ⇒ tail)
   }
 
   def empty[A]: Stream[A] = Empty
@@ -99,7 +102,23 @@ object Stream {
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
-  def from(n: Int): Stream[Int] = sys.error("todo")
 
-  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = sys.error("todo")
+  def constant[A](a: A): Stream[A] = cons(a, constant(a))
+
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+
+  def fibs_first_try: Stream[Int] = {
+      def rest(prev1: Int, prev2: Int): Stream[Int] = {
+        val next = prev1 + prev2
+        cons(next, rest(prev2, next))
+      }
+    cons(0, cons(1, rest(0,1)))
+  }
+
+  val fibs = {
+      def go(p0: Int, p1: Int): Stream[Long] = cons(p0, go(p1, p0 + p1))
+    go(0,1)
+  }
+
+  def unfold[A, S](z: S)(f: S ⇒ Option[(A, S)]): Stream[A] = sys.error("todo")
 }
--- exercises/src/test/scala/fpinscala/laziness/StreamSpec.scala
+++ exercises/src/test/scala/fpinscala/laziness/StreamSpec.scala
@@ -1,9 +1,11 @@
 package fpinscala.laziness
 
+import fpinscala.gettingstarted.MyModule._
 import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.{Matchers, PropSpec}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalactic.TypeCheckedTripleEquals._
+import Arbitrary._
 
 import scala.annotation.tailrec
 
@@ -17,95 +19,124 @@ class StreamSpec extends PropSpec with GeneratorDrivenPropertyChecks with Matche
 
   def genNonEmpty(level: Int): Gen[Stream[Int]] =
     for {
-      h <- Arbitrary.arbitrary[Int]
-      t <- genStream(level + 1)
+      h ← arbitrary[Int]
+      t ← genStream(level + 1)
     } yield Stream.cons(h, t)
 
   val streams: Gen[Stream[Int]] = genStream(0)
-
 
   property("stream toList is structurally equal to original") {
       @tailrec
       def streamEqualsList(strm: Stream[Int], list: List[Int]): Boolean =
         list match {
-          case Nil => strm == Stream.empty
-          case first :: rest =>
-            strm.headOption forall (h => h == first)
+          case Nil ⇒ strm == Stream.empty
+          case first :: rest ⇒
+            strm.headOption forall (h ⇒ h == first)
             strm match {
-              case Empty => false
-              case Cons(h, t) => streamEqualsList(t(), rest)
+              case Empty      ⇒ false
+              case Cons(h, t) ⇒ streamEqualsList(t(), rest)
             }
-      }
+        }
 
-    forAll(streams) { strm =>
+    forAll(streams) { strm ⇒
       streamEqualsList(strm, strm.toList) should ===(true)
     }
   }
 
   property("drop(n) is equivalent to List.drop(n)") {
-    forAll(streams, Gen.choose(0, 30)) { (strm, num) =>
-      (strm drop num).toList should === (strm.toList drop num)
+    forAll(streams, Gen.choose(0, 30)) { (strm, num) ⇒
+      (strm drop num).toList should ===(strm.toList drop num)
     }
   }
 
   property("take(n) is equivalent to List.take(n)") {
-    forAll(streams, Gen.choose(0, 30)) { (strm, num) =>
-      (strm take num).toList should === (strm.toList take num)
+    forAll(streams, Gen.choose(0, 30)) { (strm, num) ⇒
+      (strm take num).toList should ===(strm.toList take num)
     }
   }
 
   property("takeWhile is equivalent to List.takeWhile") {
-    forAll(streams) { strm =>
-      val p: (Int => Boolean) = _ % 2 == 0
-      (strm takeWhile p).toList should === (strm.toList takeWhile p)
+    forAll(streams) { strm ⇒
+      val p: (Int ⇒ Boolean) = _ % 2 == 0
+      (strm takeWhile p).toList should ===(strm.toList takeWhile p)
     }
   }
 
   property("forAll is equivalent to List.forall") {
-    forAll(streams) { strm =>
-      val p: (Int => Boolean) = _ % 2 == 0
-      (strm forAll p) should === (strm.toList forall p)
+    forAll(streams) { strm ⇒
+      val p: (Int ⇒ Boolean) = _ % 2 == 0
+      (strm forAll p) should ===(strm.toList forall p)
     }
   }
 
   property("takeWhileUsingFoldRight is equivalent to List.takeWhile") {
-    forAll(streams) { strm =>
-      val p: (Int => Boolean) = _ % 2 == 0
-      (strm takeWhileUsingFoldRight p).toList should === (strm.toList takeWhile p)
+    forAll(streams) { strm ⇒
+      val p: (Int ⇒ Boolean) = _ % 2 == 0
+      (strm takeWhileUsingFoldRight p).toList should ===(strm.toList takeWhile p)
     }
   }
 
   property("headOptionUsingFoldRight is equivalent to List.headOption") {
-    forAll(streams) { strm =>
-      strm.headOptionUsingFoldRight should === (strm.toList.headOption)
+    forAll(streams) { strm ⇒
+      strm.headOptionUsingFoldRight should ===(strm.toList.headOption)
     }
   }
 
   property("map is equivalent to List.map") {
-    forAll(streams) { strm =>
-      val f: (Int => String) = _.toString
-      (strm map f).toList should === (strm.toList map f)
+    forAll(streams) { strm ⇒
+      val f: (Int ⇒ String) = _.toString
+      (strm map f).toList should ===(strm.toList map f)
     }
   }
 
   property("filter is equivalent to List.filter") {
-    forAll(streams) { strm =>
-      val p: (Int => Boolean) = _ % 2 == 0
-      (strm filter p).toList should === (strm.toList filter p)
+    forAll(streams) { strm ⇒
+      val p: (Int ⇒ Boolean) = _ % 2 == 0
+      (strm filter p).toList should ===(strm.toList filter p)
     }
   }
 
   property("append is equivalent to List.++") {
-    forAll(streams, streams) { (strm1, strm2) =>
-      (strm1 append strm2).toList should === (strm1.toList ++ strm2.toList)
+    forAll(streams, streams) { (strm1, strm2) ⇒
+      (strm1 append strm2).toList should ===(strm1.toList ++ strm2.toList)
     }
   }
 
   property("flatMap is equivalent to List.map") {
-    forAll(streams) { strm =>
-      val fStream: (Int => Stream[String]) = n => Stream.cons(n.toString, Stream.empty)
-      val fList: (Int => List[String]) = n => n.toString :: List.empty
-      (strm flatMap fStream).toList should === (strm.toList flatMap fList)
+    forAll(streams) { strm ⇒
+      val fStream: (Int ⇒ Stream[String]) = n ⇒ Stream.cons(n.toString, Stream.empty)
+      val fList: (Int ⇒ List[String]) = n ⇒ n.toString :: List.empty
+      (strm flatMap fStream).toList should ===(strm.toList flatMap fList)
+    }
+  }
+
+  property("infinite stream of a constant") {
+    forAll(Gen.choose(0, 100), arbitrary[Int]) { (take, const) ⇒
+      Stream.constant(const).take(take).toList should ===(List.fill(take)(const))
+    }
+  }
+
+  property("infinite stream counting up from") {
+      def check(countdown: Int, expected: Int, stream: Stream[Int]): Unit =
+        if (countdown == 0)
+          stream.headOption should ===(None)
+        else {
+          stream.headOption should ===(Some(expected))
+          check(countdown - 1, expected + 1, stream match {
+            case Empty      ⇒ fail("stream should not be empty")
+            case Cons(h, t) ⇒ t()
+          })
+        }
+
+    forAll(Gen.choose(0, 100), arbitrary[Int]) { (take, start) ⇒
+      val list = Stream.from(start).take(take)
+      check(take, start, list)
+    }
+  }
+
+  property("verify up to 200 fibs") {
+    forAll(Gen.choose(1, 200)) { n =>
+      Stream.fibs.take(n).toList should === (((1 to n) map (i => fib(i))).toList)
     }
   }
 }
