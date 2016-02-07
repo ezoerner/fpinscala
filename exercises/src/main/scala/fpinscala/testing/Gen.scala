c21c9223b1b9e58b90199c1efb41af333c7cd6aa
Eric Zoerner <eric.zoerner@gmail.com>
2016-02-06 16:53:11 -0800

    8.4

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -1,12 +1,6 @@
 package fpinscala.testing
 
-import fpinscala.laziness.Stream
 import fpinscala.state._
-import fpinscala.parallelism._
-import fpinscala.parallelism.Par.Par
-import Gen._
-import Prop._
-import java.util.concurrent.{Executors,ExecutorService}
 
 /*
 The library developed in this chapter goes through several iterations. This file is just the
@@ -28,14 +22,29 @@ object Prop {
 
 object Gen {
   def unit[A](a: => A): Gen[A] = ???
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+    val nonNegativeInt: (RNG) ⇒ (Int, RNG) = RNG.nonNegativeInt
+    val state: State[RNG, Int] = State(nonNegativeInt)
+    Gen(state.map(n => start + n % (stopExclusive-start)))
+  }
+
+  // with implicit types, this is simplified to:
+/*
+  Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive-start)))
+*/
 }
 
+/*
 trait Gen[A] {
   def map[A,B](f: A => B): Gen[B] = ???
   def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
 }
+*/
 
 trait SGen[+A] {
 
 }
 
+case class Gen[A](sample: State[RNG,A]) {
+}
