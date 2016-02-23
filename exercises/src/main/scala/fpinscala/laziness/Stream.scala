46423a2b74ed113312e8649d589d94826e2182f6
Eric Zoerner <eric.zoerner@gmail.com>
2016-02-22 21:08:20 -0800

    8.13 Reorg and SGen.listOf1

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -144,6 +144,10 @@ sealed trait Stream[+A] {
       case _                            ⇒ None
     }
 
+  // special case of `zip`
+  def zip[B](s2: Stream[B]): Stream[(A,B)] =
+    zipWith(s2)((_,_))
+
   // TODO test this vs the version in the answers
   def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] = {
     unfold((this, s2)) {
--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -1,16 +1,50 @@
 package fpinscala.testing
 
+import fpinscala.laziness.Stream
+import fpinscala.parallelism.Par.Par
+import fpinscala.parallelism._
 import fpinscala.state._
+import fpinscala.testing.Prop._
+
+import scala.language.{implicitConversions, postfixOps}
 
 /*
 The library developed in this chapter goes through several iterations. This file is just the
 shell, which you can fill in and modify while working through the chapter.
 */
 
+case class Prop(run: (MaxSize, TestCases, RNG) ⇒ Result) {
+  def &&(p: Prop): Prop = Prop { (maxSize, testCases, rng) ⇒
+    run(maxSize, testCases, rng) match {
+      case Passed ⇒ p.run(maxSize, testCases, rng)
+      case result ⇒ result
+    }
+  }
+
+  def ||(p: Prop): Prop = Prop { (maxSize, testCases, rng) ⇒
+    run(maxSize, testCases, rng) match {
+      case Falsified(msg, _) ⇒
+        p.tag(msg).run(maxSize, testCases, rng)
+      case result ⇒ result
+    }
+  }
+
+  /** This is rather simplistic - in the event of failure, we simply prepend
+   *  the given message on a newline in front of the existing message.
+   */
+  def tag(msg: String) = Prop {
+    (max, n, rng) => run(max, n, rng) match {
+      case Falsified(e, c) => Falsified(msg + "\n" + e, c)
+      case x => x
+    }
+  }
+}
+
 object Prop {
-  type FailedCase = String
   type SuccessCount = Int
   type TestCases = Int
+  type MaxSize = Int
+  type FailedCase = String
 
   sealed trait Result {
     def isFalsified: Boolean
@@ -25,25 +59,87 @@ object Prop {
     def isFalsified = true
   }
 
-  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+  /* Produce an infinite random stream from a `Gen` and a starting `RNG`. */
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (n,rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
+
+  // String interpolation syntax. A string starting with `s"` can refer to
+  // a Scala value `v` as `$v` or `${v}` in the string.
+  // This will be expanded to `v.toString` by the Scala compiler.
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+      s"generated an exception: ${e.getMessage}\n" +
+      s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+  def apply(f: (TestCases,RNG) => Result): Prop =
+    Prop { (_,n,rng) => f(n,rng) }
+
+  def forAll[A](g: SGen[A])(f: A => Boolean): Prop =
+    forAll(g(_))(f)
+
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop {
+    (max,n,rng) =>
+      val casesPerSize = (n - 1) / max + 1
+      val props: Stream[Prop] =
+        Stream.from(0).take((n min max) + 1).map(i => forAll(g(i))(f))
+      val prop: Prop =
+        props.map(p => Prop { (max, n, rng) =>
+          p.run(max, casesPerSize, rng)
+        }).toList.reduce(_ && _)
+      prop.run(max,n,rng)
+  }
+
+  def run(p: Prop,
+          maxSize: Int = 100,
+          testCases: Int = 100,
+          rng: RNG = RNG.Simple(System.currentTimeMillis)): Unit =
+    p.run(maxSize, testCases, rng) match {
+      case Falsified(msg, n) =>
+        println(s"! Falsified after $n passed tests:\n $msg")
+      case Passed =>
+        println(s"+ OK, passed $testCases tests.")
+    }
+
+  def check(p: => Boolean): Prop = Prop { (_, _, _) =>
+    if (p) Passed else Falsified("()", 0)
+  }
+
+  def equal[A](p: Par[A], p2: Par[A]): Par[Boolean] =
+    Par.map2(p,p2)(_ == _)
 }
 
-import Prop._
-case class Prop(run: (TestCases, RNG) ⇒ Result) {
-  def &&(p: Prop): Prop = Prop { (testCases, rng) ⇒
-    run(testCases, rng) match {
-      case Passed ⇒ p.run(testCases, rng)
-      case result ⇒ result
-    }
-  }
+case class Gen[A](sample: State[RNG,A]) {
+  /** Exercise 8.6
+    *   Implement flatMap, and then use it to implement this more dynamic
+    *   version of listOfN. Put flatMap and listOfN in the Gen class.
+    */
+  def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample flatMap (a ⇒ f(a).sample))
 
-  def ||(p: Prop): Prop = Prop { (testCases, rng) ⇒
-    run(testCases, rng) match {
-      case Falsified(_, _) ⇒
-        p.run(testCases, rng)
-      case result ⇒ result
-    }
-  }
+  def map[B](f: A => B): Gen[B] = Gen(sample map f)
+
+  def listOfN(size: Int): Gen[List[A]] =
+    Gen.listOfN(size, this)
+
+  /* A version of `listOfN` that generates the size to use dynamically. */
+  def listOfN(size: Gen[Int]): Gen[List[A]] =
+    size flatMap (n => this.listOfN(n))
+
+  def listOf: SGen[List[A]] = Gen.listOf(this)
+  def listOf1: SGen[List[A]] = Gen.listOf1(this)
+
+  /** =Exercise 8.10=
+    * Implement helper functions for converting Gen to SGen.
+    * You can add this as a method on Gen.
+    */
+  def unsized: SGen[A] = SGen(_ ⇒ this)
 }
 
 object Gen {
@@ -92,38 +188,6 @@ object Gen {
     val weight1 = g1._2 / totalWeight
     double flatMap (d ⇒ if (d <= weight1) g1._1 else g2._1)
   }
-}
-
-
-case class Gen[A](sample: State[RNG,A]) {
-  /** Exercise 8.6
-    *   Implement flatMap, and then use it to implement this more dynamic
-    *   version of listOfN. Put flatMap and listOfN in the Gen class.
-    */
-  def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample flatMap (a ⇒ f(a).sample))
-
-  def map[B](f: A => B): Gen[B] = Gen(sample map f)
-
-  def listOfN(size: Gen[Int]): Gen[List[A]] =
-    size flatMap { n ⇒
-      Gen.listOfN(n, this)
-    }
-
-  /** Exercise 8.10
-    * Implement helper functions for converting Gen to SGen.
-    * You can add this as a method on Gen.
-    */
-  def unsized: SGen[A] = SGen(_ ⇒ this)
-}
-
-/*
-// This is the same as:
-case class Gen[A](sample: Rand[A])
-*/
-
-case class SGen[+A](forSize: Int ⇒ Gen[A])
-
-object SGen {
 
   /** Exercise 8.12
     * Implement a listOf combinator that doesn’t accept an explicit size.
@@ -131,4 +195,26 @@ object SGen {
     * generate lists of the requested size.
     */
   def listOf[A](g: Gen[A]): SGen[List[A]] = SGen(n ⇒ g.listOfN(Gen.unit(n)))
+
+  /** =Exercise 8.13=
+    * Define listOf1 for generating nonempty lists, and then update your
+    * specification of max to use this generator.
+    */
+  def listOf1[A](g: Gen[A]): SGen[List[A]] = SGen(n ⇒ g.listOfN(Gen.unit(n max 1)))
+
+  val smallInt = Gen.choose(-10, 10)
+  val maxProp = forAll(listOf1(smallInt)) { l =>
+    val max = l.max
+    !l.exists(_ > max) // No value greater than `max` should exist in `l`
+  }
+}
+
+case class SGen[A](g: Int => Gen[A]) {
+  def apply(n: Int): Gen[A] = g(n)
+
+  def map[B](f: A => B): SGen[B] =
+    SGen(g andThen (_ map f))
+
+  def flatMap[B](f: A => Gen[B]): SGen[B] =
+    SGen(g andThen (_ flatMap f))
 }
